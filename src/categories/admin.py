3d4141c0b1dc8b7dc296a7db25bb1d6895f70417
Prohodchikoff <ivanecdima63@gmail.com>
2026-08-17 16:54:27 +0300

    Refactor: Clean up imports and formatting across multiple files

--- src/categories/admin.py
+++ src/categories/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from mptt.admin import MPTTModelAdmin
+
 from .models import Categories
 
 admin.site.register(Categories, MPTTModelAdmin)
--- src/categories/models.py
+++ src/categories/models.py
@@ -7,9 +7,7 @@ from mptt.models import MPTTModel, TreeForeignKey
 class Categories(MPTTModel):
     name = models.CharField(max_length=50)
     slug = models.SlugField(unique=True, blank=True)
-    parent = TreeForeignKey(
-        'self', on_delete=models.CASCADE, null=True, blank=True, related_name='children'
-    )
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
 
     class MPTTMeta:
         order_insertion_by = ['name']
